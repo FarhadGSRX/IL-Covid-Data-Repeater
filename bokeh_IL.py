9ae8c308aa68682c288216d21b75c26278de35db
FarhadGSRX <farhad.ghamsari@gmail.com>
2020-04-23 00:07:05 -0500

    Saving a few more lines of code.

--- bokeh_IL.py
+++ bokeh_IL.py
@@ -110,3 +110,39 @@ from bokeh.models import HoverTool
 
 hover = HoverTool(tooltips=None, mode='hline')
 plot = figure(tools=[hover, 'crosshair'])
+
+# Some helpful code lines
+from bokeh.models.widgets import Tabs, Panel
+
+first = Panel(child=row(p1, p2), title="first")
+second = Panel(child=row(p3), title="Second")
+
+# Put panels in a tabs object
+tabs = Tabs(tabs=[first, second])
+
+== == == =
+# Import Panel from bokeh.models.widgets
+from bokeh.models.widgets import Panel
+
+# Create tab1 from plot p1: tab1
+tab1 = Panel(child=p1, title='Latin America')
+
+# Create tab2 from plot p2: tab2
+tab2 = Panel(child=p2, title="Africa")
+
+# Create tab3 from plot p3: tab3
+tab3 = Panel(child=p3, title="Asia")
+
+# Create tab4 from plot p4: tab4
+tab4 = Panel(child=p4, title="Europe")
+== == == == ==
+
+# Import Tabs from bokeh.models.widgets
+from bokeh.models.widgets import Tabs
+
+# Create a Tabs layout: layout
+layout = Tabs(tabs=[tab1, tab2, tab3, tab4])
+
+# Specify the name of the output_file and show the result
+output_file('tabs.html')
+show(layout)
